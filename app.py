07b5bba322087218a7f9435f63b5cbff136449be
Messiah64 <87025466+Messiah64@users.noreply.github.com>
2024-09-24 17:27:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ def generate_docx(text):
 
 st.header("S.A.R.A AI  &nbsp;&nbsp;:brain: :calendar: :zap:")
 
-st.text("Structured Assesment & Review Aid")
+st.text("**Structured Assesment & Review Aid**")
 st.text("No more fretting over upcoming Audits or Course Exams")
 st.text("Stay up to date with new revised Standard Operating Procedure")
 
